0eb27a8292581bbcb75c85d81f8578adc91a43d5
Ceenia <Ceenia@USER-PC>
2019-03-15 14:04:40 +1300

    Commitin .net project

---
+++ Students/Controllers/TrialController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Students.Controllers
+{
+    public class TrialController : Controller
+    {
+        // get: student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+        //public string index(string name, int numTimes = 1)
+        //{
+        //    return HttpUtility.HtmlAttributeEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        //}
+    }
+}
---
+++ Students/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Students.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime Dob { get; set; }
+        public string Gender { get; set; }
+        public decimal Mark { get; set; }
+    }
+
+    public class StudentDBContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
